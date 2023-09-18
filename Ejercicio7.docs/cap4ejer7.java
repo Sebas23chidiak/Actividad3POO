9280e1d3322b6ef6d6239f9e2d98094c5680795e
Sebas23chidiak <143117455+Sebas23chidiak@users.noreply.github.com>
2023-09-18 16:13:12 -0500

    Create cap4ejer7.java

---
+++ Ejercicio7.docs/cap4ejer7.java
@@ -0,0 +1,9 @@
+package cap4ejer7;
+
+
+public class Res7cap4 {
+    public static void main(String[] args) {
+        interfaz ventana = new interfaz();
+        ventana.setVisible(true);
+    }
+}
