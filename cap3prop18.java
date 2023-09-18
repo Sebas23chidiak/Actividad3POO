03078895607b517f6fd9534a3586c37e3f4da209
Sebas23chidiak <143117455+Sebas23chidiak@users.noreply.github.com>
2023-09-18 15:08:27 -0500

    Create cap3prop18.java

---
+++ cap3prop18.java
@@ -0,0 +1,9 @@
+package prop18cap3;
+
+public class Prop18cap3 {
+
+    public static void main(String[] args) {
+        interfazUsuario ventana = new interfazUsuario();
+        ventana.setVisible(true);
+    }
+}
