f384f9747ebcceff981d3215f01b51576f6c78d6
Sebas23chidiak <143117455+Sebas23chidiak@users.noreply.github.com>
2023-09-18 18:03:33 -0500

    Create figuras geometricas.java

---
+++ Parte2.docs/figuras geometricas.java
@@ -0,0 +1,8 @@
+package figurasgeometricas;
+
+public class FigurasGeometricas {
+    public static void main(String[] args) {
+        interfazPrincipal ventana = new interfazPrincipal();
+        ventana.setVisible(true);
+    }
+}
