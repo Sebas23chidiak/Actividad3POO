07665f44dd80e0844c082b1eff0d2d8a22870d33
Sebas23chidiak <143117455+Sebas23chidiak@users.noreply.github.com>
2023-09-18 16:16:47 -0500

    Update cap4ejer7.java

--- Ejercicio7.docs/cap4ejer7.java
+++ Ejercicio7.docs/cap4ejer7.java
@@ -1,7 +1,7 @@
 package cap4ejer7;
 
 
-public class Res7cap4 {
+public class Cap4ejer7 {
     public static void main(String[] args) {
         interfaz ventana = new interfaz();
         ventana.setVisible(true);
