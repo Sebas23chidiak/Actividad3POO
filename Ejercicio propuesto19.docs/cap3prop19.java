698f947f1abc55b5b189e0aa39a5ed29862e670d
Sebas23chidiak <143117455+Sebas23chidiak@users.noreply.github.com>
2023-09-18 15:50:39 -0500

    Create cap3prop19.java

---
+++ Ejercicio propuesto19.docs/cap3prop19.java
@@ -0,0 +1,12 @@
+
+package cap3prop19;
+
+
+public class Prop19cap3 {
+
+    public static void main(String[] args) {
+        interfaz ventana = new interfaz();
+        ventana.setVisible(true);
+    }
+    
+}
